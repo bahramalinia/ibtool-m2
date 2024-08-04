38c727b6ac5086ec46fbe71b3102292651e01617
Bahram Alinia <alinia.b@gmail.com>
2024-08-04 23:25:54 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -252,7 +252,7 @@ data "aws_availability_zones" "available" {}
 
 locals {
   region = "eu-west-3"
-  name   = "ex-${basename(path.cwd)}"
+  name   = "ibtool"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
